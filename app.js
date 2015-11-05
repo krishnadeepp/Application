0262b314f2e1ead5ddaf90a4eb3defbc036c923e
krishnadeep <krishnadeepp@gmail.com>
2015-11-05 12:16:13 +0530

    Node with Express

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-// Load the necessary servers.
-var sys = require( "sys" );
-var http = require( "http" );
- 
-// Create our HTTP server.
-var server = http.createServer(
-function( request, response ){
- 
- 
-// Create a SUPER SIMPLE response.
-response.writeHead( 200, {"content-type": "text/plain"} );
-response.write( "Hellow world from AWS!\n" );
-response.end();
- 
- 
-}
-);
- 
-// Point the HTTP server to port 8080.
-server.listen( 8080 );
- 
-// For logging....
-sys.puts( "Server is running on 8080" );
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
