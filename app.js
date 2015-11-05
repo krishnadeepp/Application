f1ad622a122e632f1bedd0ac412dc95ac38b9aeb
krishnadeep <krishnadeepp@gmail.com>
2015-11-05 11:49:05 +0530

    1

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+// Load the necessary servers.
+var sys = require( "sys" );
+var http = require( "http" );
+ 
+// Create our HTTP server.
+var server = http.createServer(
+function( request, response ){
+ 
+ 
+// Create a SUPER SIMPLE response.
+response.writeHead( 200, {"content-type": "text/plain"} );
+response.write( "Hellow world from AWS!\n" );
+response.end();
+ 
+ 
+}
+);
+ 
+// Point the HTTP server to port 8080.
+server.listen( 8080 );
+ 
+// For logging....
+sys.puts( "Server is running on 8080" );
