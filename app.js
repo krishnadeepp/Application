f5d0afd58ae0ae6fd4e58bb9cac7955bd4221d48
krishnadeep <krishnadeepp@gmail.com>
2015-11-06 11:07:17 +0530

    8000

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 app.get('/test', function (req, res) {
   res.send('test');
 });
-var server = app.listen(8081, function () {
+var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
