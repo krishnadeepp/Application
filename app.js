e53c239eec11de5476dbeeb1748ed8f4adb20cf8
krishnadeep <krishnadeepp@gmail.com>
2015-11-06 08:57:04 +0530

    dfsdf

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
+	 console.log('in server');
   res.send('Hello World!');
 });
 app.get('/test', function (req, res) {
