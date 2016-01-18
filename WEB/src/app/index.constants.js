9dac162bcbb5636ad9cd42423012de6e222626aa
krishnadeep <krishnadeepp@gmail.com>
2016-01-18 10:30:21 +0530

    adding server url

--- WEB/src/app/index.constants.js
+++ WEB/src/app/index.constants.js
@@ -6,6 +6,7 @@
     .module('app')
     .constant('malarkey', malarkey)
     .constant('moment', moment)
-    .constant('BaseUrl', 'http://localhost:8081');
+    //.constant('BaseUrl', 'http://localhost:8081');
+    .constant('BaseUrl', 'http://ec2-52-32-227-158.us-west-2.compute.amazonaws.com:8081/');
 
 })();
