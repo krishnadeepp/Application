9aed8a14c9259c79b99bac599030fec4fdee3b68
krishnadeep <krishnadeepp@gmail.com>
2016-01-18 10:45:32 +0530

    url change

--- WEB/src/app/index.constants.js
+++ WEB/src/app/index.constants.js
@@ -7,6 +7,6 @@
     .constant('malarkey', malarkey)
     .constant('moment', moment)
     //.constant('BaseUrl', 'http://localhost:8081');
-    .constant('BaseUrl', 'http://ec2-52-32-227-158.us-west-2.compute.amazonaws.com:8081/');
+    .constant('BaseUrl', 'http://ec2-52-32-227-158.us-west-2.compute.amazonaws.com:8081');
 
 })();
