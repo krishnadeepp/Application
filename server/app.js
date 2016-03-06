a1d9047c643e44422151f31ccf29f0d2074a2bca
krishnadeep <kpuchalapalli@QSIH-MAC27.local>
2016-03-06 11:52:40 +0530

    removed mongodb

--- server/app.js
+++ server/app.js
@@ -5,11 +5,11 @@ var bodyParser = require('body-parser');
 
 //custom imports
 var conf = require('./lib/config/conf.js');
-var db = require('./lib/database/connection');
+//var db = require('./lib/database/connection');
 
 
 var app = express();
-var dbconnection = conf.dbconnection.server + "://"+conf.dbconnection.ip+":"+conf.dbconnection.port+"/"+conf.dbconnection.database;
+//var dbconnection = conf.dbconnection.server + "://"+conf.dbconnection.ip+":"+conf.dbconnection.port+"/"+conf.dbconnection.database;
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(function(req, res, next) {
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-require('./routes').Route(app);
+//require('./routes').Route(app);
 var server = app.listen(conf.serverport, function () {
 var host = server.address().address;
 var port = server.address().port;
@@ -25,14 +25,18 @@ console.log('Example app listening at http://%s:%s', host, port);
 
 });
 
-db.connect(dbconnection,function(err){
-console.log(dbconnection);
-	if(err){
-		console.log(err);
-		process.exit(1);
-
-	}
-	else
-		console.log('mongodb started');
-
+app.get('/',function(req,res){
+  res.send('helloworld');
 });
+
+// db.connect(dbconnection,function(err){
+// console.log(dbconnection);
+// 	if(err){
+// 		console.log(err);
+// 		process.exit(1);
+
+// 	}
+// 	else
+// 		console.log('mongodb started');
+
+// });
